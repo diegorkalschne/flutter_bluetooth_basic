fbed5fa03a34b42915e143c3c95575a460dea9e9
Diego <diegokalschne789@gmail.com>
2024-09-05 09:42:47 -0300

    chore: update dependencies

--- lib/src/bluetooth_device.g.dart
+++ lib/src/bluetooth_device.g.dart
@@ -6,13 +6,12 @@ part of 'bluetooth_device.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-BluetoothDevice _$BluetoothDeviceFromJson(Map<String, dynamic> json) {
-  return BluetoothDevice()
-    ..name = json['name'] as String?
-    ..address = json['address'] as String?
-    ..type = json['type'] as int?
-    ..connected = json['connected'] as bool?;
-}
+BluetoothDevice _$BluetoothDeviceFromJson(Map<String, dynamic> json) =>
+    BluetoothDevice()
+      ..name = json['name'] as String?
+      ..address = json['address'] as String?
+      ..type = (json['type'] as num?)?.toInt()
+      ..connected = json['connected'] as bool?;
 
 Map<String, dynamic> _$BluetoothDeviceToJson(BluetoothDevice instance) {
   final val = <String, dynamic>{};
